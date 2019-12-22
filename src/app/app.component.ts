8523e65c1ff555adc2d80d2233f230d9dfb4dd74
Your Name <you@example.com>
2019-12-22 15:07:31 +0530

    My Commit_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fetchJSONDemo1';
+  personsArray: any = [];
+  selectedValue: any = [];
+  constructor(private http: HttpClient) {}
+
+  fetchPersons() {
+    return this.http.get('./assets/test.json').subscribe((data) => {
+      this.personsArray = data;
+      console.log(data);
+    });
+  }
 }
